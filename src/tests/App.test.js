258b5e2606f392ed485737186ec0c25337e0ddf7
Pedro Luiz <pedroluizlsg@gmail.com>
2022-08-17 20:57:54 -0300

    Req1 sim finalizado

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,22 +1,17 @@
 // npx stryker run ./stryker/nomeDoArquivo.conf.json
 // test('', () => { });
 import React from 'react';
-import { render, screen } from '@testing-library/react';
-import { createMemoryHistory } from 'history';
-import { Router } from 'react-router';
+import { screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from '../App';
+import renderWithRouter from './renderWithRouter';
 
 describe('Testando App.js', () => {
   describe('Verifica se o header tem um conjunto fixo de links para navegação', () => {
     test('Verifica se o Home existe em App', () => {
       // acessar
-      const history = createMemoryHistory();
-      render(
-        <Router history={ history }>
-          <App />
-        </Router>,
-      );
-      const linkHome = screen.getByText('Home');
+      renderWithRouter(<App />);
+      const linkHome = screen.getByRole('link', { name: 'Home' });
 
       // aferir
       expect(linkHome).toBeInTheDocument();
@@ -24,13 +19,8 @@ describe('Testando App.js', () => {
 
     test('Verifica se o About existe em App', () => {
       // acessar
-      const history = createMemoryHistory();
-      render(
-        <Router history={ history }>
-          <App />
-        </Router>,
-      );
-      const linkAbout = screen.getByText('About');
+      renderWithRouter(<App />);
+      const linkAbout = screen.getByRole('link', { name: 'About' });
 
       // aferir
       expect(linkAbout).toBeInTheDocument();
@@ -38,16 +28,43 @@ describe('Testando App.js', () => {
 
     test('Verifica se o Favorite Pokémons existe', () => {
       // acessar
-      const history = createMemoryHistory();
-      render(
-        <Router history={ history }>
-          <App />
-        </Router>,
-      );
-      const linkFp = screen.getByText('Favorite Pokémons');
+      renderWithRouter(<App />);
+      const linkFp = screen.getByRole('link', { name: 'Favorite Pokémons' });
 
       // aferir
       expect(linkFp).toBeInTheDocument();
+    });
+  });
+
+  describe('Testa os redirecionamentos da aplicação', () => {
+    test('Verifica se ao clicar em Home, a aplicação vai para a página inicial', () => {
+      const { history } = renderWithRouter(<App />);
+      const linkHome = screen.getByRole('link', { name: 'Home' });
+      expect(linkHome).toBeInTheDocument();
+
+      const { location: { pathname } } = history;
+      userEvent.click(linkHome);
+      expect(pathname).toBe('/');
+    });
+
+    test('Verifica se ao clicar em About, a aplicação vai para a página about', () => {
+      const { history } = renderWithRouter(<App />);
+      const linkAbout = screen.getByRole('link', { name: 'About' });
+      expect(linkAbout).toBeInTheDocument();
+
+      // const { location: { pathname } } = history;
+      userEvent.click(linkAbout);
+      expect(history.location.pathname).toBe('/about');
+    });
+
+    test('Se ao clicar em Favorite Pokémons, redireciona para /FavoritePokemons', () => {
+      const { history } = renderWithRouter(<App />);
+      const linkFavorites = screen.getByRole('link', { name: 'Favorite Pokémons' });
+      expect(linkFavorites).toBeInTheDocument();
+
+      // const { location: { pathname } } = history;
+      userEvent.click(linkFavorites);
+      expect(history.location.pathname).toBe('/favorites');
     });
   });
 });
